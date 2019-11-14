4f6e529143e16e3805d24fdf563d2f1f260b19c0
Your Name <2958521571@qq.com>
2019-11-14 21:02:53 +0800

    改了controller

--- product_service/src/main/java/com/bw/controller/BuyerProductController.java
+++ product_service/src/main/java/com/bw/controller/BuyerProductController.java
@@ -5,6 +5,7 @@ import com.bw.vo.BuyerProductInfoVO;
 import com.bw.vo.CodeMsg;
 import com.bw.vo.ResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/buyer/product")
+@CrossOrigin
 public class BuyerProductController {
 
     @Autowired
